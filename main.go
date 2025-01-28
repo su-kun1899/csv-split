cfaa2ce59237883e3361457fc857ef9174c0002f
su-kun1899 <higedrum.coz@gmail.com>
2025-01-28 15:22:06 +0900

    feat: :wrench: main の枠組みを作成

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Usage: csv-split <input-file> <lines-per-file>")
-		return
+var commandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+// run is the entry point for the csv-split command.
+func run(args []string) int {
+	recordCount := commandLine.Int("record_count", 100, "Create split files record_count records.")
+	if err := commandLine.Parse(args); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot parse flags: %v\n", err)
 	}
-	fmt.Println("CSV Splitter CLI")
+
+	if commandLine.NArg() > 1 {
+		fmt.Fprintln(os.Stderr, "Only one file can be processed at a time. Usage: csv-split <file>")
+		return 1
+	}
+	fileName := commandLine.Arg(0)
+
+	fmt.Printf("record_count: %v\n", *recordCount)
+	fmt.Printf("file_name: %v\n", fileName)
+
+	return 0
+}
+
+func main() {
+	os.Exit(run(os.Args[1:]))
 }
